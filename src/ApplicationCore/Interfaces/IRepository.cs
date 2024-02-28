bd9aea1cba533c1ea6176ade3a22d9059a97bc65
hakanpakelgil <hakanpakelgil@hotmail.com>
2024-02-28 12:31:48 +0300

    IRepository,Configs

---
+++ src/ApplicationCore/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using ApplicationCore.Entities;
+using Ardalis.Specification;
+
+namespace ApplicationCore.Interfaces
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        public Task<T?> GetByIdAsync(int id);
+        public Task<List<T>> GetAllAsync(ISpecification<T> specification);
+        public Task<T> AddAsync(T entity);
+        public Task UpdateAsync(T entity);
+        public Task DeleteAsync(T entity);
+        public Task<int> CountAsync (ISpecification<T> specification);
+        public Task<T> FirstAsync (ISpecification<T> specification);
+        public Task<T?> FirstorDefaultAsync (ISpecification<T> specification);
+    }
+}
