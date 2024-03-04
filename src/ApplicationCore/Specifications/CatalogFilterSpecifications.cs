0376ca24bb8aae65377f52a87e7050315ce9a64b
hakanpakelgil <hakanpakelgil@hotmail.com>
2024-03-04 12:30:01 +0300

    home view model

---
+++ src/ApplicationCore/Specifications/CatalogFilterSpecifications.cs
@@ -0,0 +1,22 @@
+﻿using ApplicationCore.Entities;
+using Ardalis.Specification;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Specifications
+{
+    public class CatalogFilterSpecifications : Specification<Product>
+    {
+        public CatalogFilterSpecifications(int? categoryId, int? brandId)
+        {
+            if (categoryId.HasValue)
+                Query.Where(x => x.CategoryId == categoryId);
+
+            if (brandId.HasValue)
+                Query.Where(x => x.BrandId == brandId);
+        }
+    }
+}
