82b4dbf11471cd9811ec73fa7c5882b527810396
hakanpakelgil <hakanpakelgil@hotmail.com>
2024-03-06 12:15:38 +0300

    basket service

---
+++ src/ApplicationCore/Interfaces/IBasketService.cs
@@ -0,0 +1,19 @@
+﻿using ApplicationCore.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Interfaces
+{
+    public interface IBasketService
+    {
+        public Task<Basket> GetOrCreateBasketAsync(string buyerId);
+        public Task<Basket> AddItemToBasketAsync(string buyerId,int productId,int quantity);
+        public Task<Basket> SetQuantitiesAsync(string buyerId,Dictionary<int,int> quantities);
+        public Task DeleteBasketItemAsync(string buyerId,int productId);
+        public Task EmptyBasketAsync(string buyerId);
+        public Task TransferBasketAsync(string sourceBuyerId, string destinationBuyerId);
+    }
+}
